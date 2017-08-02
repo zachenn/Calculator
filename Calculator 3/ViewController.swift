d9552f71af52c2970c885ba8b8d97774e8c60bc0
Zach <myemail757@gmail.com>
2017-08-01 22:22:03 -0700

    Initial Commit

---
+++ Calculator 3/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Calculator 3
+//
+//  Created by Zach Chen on 7/30/17.
+//  Copyright © 2017 Zach Chen. All rights reserved.
+//
+
+import UIKit
+
+enum modes {
+	
+	case not_set
+	case addition
+	case subtraction
+	case multiplication
+	
+}
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var label: UILabel!
+	
+	var labelString: String = "0"
+	var lastButtonWasMode: Bool = false
+	var currentMode: modes = .not_set
+	var savedNum: Int = 0
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func didPressPlus(_ sender: Any) {
+		changeMode (newMode: .addition)
+	}
+	
+	@IBAction func didPressSubtract(_ sender: Any) {
+		changeMode(newMode: .subtraction)
+	}
+	
+	@IBAction func didPressMultiply(_ sender: Any) {
+		changeMode(newMode: .multiplication)
+	}
+
+	@IBAction func didPressEquals(_ sender: Any) {
+		guard let labelInt: Int = Int(labelString) else {
+			return
+		}
+		if (currentMode == .not_set || lastButtonWasMode) {
+			return
+		}
+		else if (currentMode == .addition) {
+			savedNum += labelInt
+		}
+		else if (currentMode == .subtraction) {
+			savedNum -= labelInt
+		}
+		else if (currentMode == .multiplication) {
+			savedNum *= labelInt
+		}
+		currentMode = .not_set
+		labelString = "\(savedNum)"
+		lastButtonWasMode = false
+		updateText()
+	}
+	
+	@IBAction func didPressClear(_ sender: Any) {
+		
+		labelString = "0"
+		lastButtonWasMode = false
+		currentMode = .not_set
+		savedNum = 0
+		label.text = "0"
+		
+	}
+	
+	@IBAction func didPressNumber(_ sender: UIButton) {
+		let stringValue: String? = sender.titleLabel?.text
+		if (lastButtonWasMode) {
+			lastButtonWasMode = false
+			labelString = "0"
+		}
+		labelString = labelString.appending(stringValue!)
+		updateText()
+	}
+	
+	func updateText() {
+		guard let labelInt: Int = Int(labelString) else {
+			return
+		}
+		if (currentMode == .not_set) {
+			savedNum = labelInt
+		}
+		let formatter: NumberFormatter = NumberFormatter()
+		formatter.numberStyle = .decimal
+		let num: NSNumber = NSNumber (value: labelInt)
+		label.text = formatter.string(from:num)
+	}
+	
+	func changeMode (newMode: modes) {
+		if (savedNum == 0) {
+			return
+		}
+		currentMode = newMode
+		lastButtonWasMode = true
+	}
+}
+
---
+++ Calculator 3Tests/Calculator_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Calculator_3Tests.swift
+//  Calculator 3Tests
+//
+//  Created by Zach Chen on 7/30/17.
+//  Copyright © 2017 Zach Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_3
+
+class Calculator_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
